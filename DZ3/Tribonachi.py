656b8e2bf3fb6cf346d2b6a3658f626c79390fbd
Eroshkin-IBA <eroshkin.artyom.1.21@gmail.com>
2022-06-26 11:32:39 +0300

    dz2

--- DZ3/Tribonachi.py
+++ DZ3/Tribonachi.py
@@ -1,12 +1,20 @@
+a = 0
+
 def tribonachi(i):
+    global a
+    a +=1
+
+    print(a)
     if(i==1 or i==2):
         return 0
     if(i==3 or i==4):
         return 1
     return tribonachi(i-1)+tribonachi(i-2)+tribonachi(i-3)
-a=[]
-#s = int(input("Введите число "))
-for i in range(1,35):
-    a.append(tribonachi(i))
 
-print(a)
+
+b = []
+#s = int(input("Введите число "))
+for i in range(1,20):
+    b.append(tribonachi(i))
+
+print(b)
